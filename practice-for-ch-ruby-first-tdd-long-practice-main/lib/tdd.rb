b61e37470e4ee89d5d53df8131d1cfe857d071ea
harjitsingh3 <singh.harjit09@gmail.com>
2023-03-31 15:11:46 -0400

    Create two_sum rspec

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -7,3 +7,17 @@ def my_uniq(array)
     end
     new_arr
 end
+
+class Array
+    def two_sum 
+        new_arr = []
+        self.each_with_index do |ele1, i|
+            self.each_with_index do |ele2, j|
+                if j > i && ele1 + ele2 == 0 
+                    new_arr << [i, j]
+                end
+            end
+        end
+        new_arr 
+    end
+end
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -2,7 +2,7 @@ require 'tdd'
 require 'rspec'
 
 # describe Array do 
-    describe "my_uniq" do 
+    describe "#my_uniq" do 
         it "should accept an array as an argument" do 
             expect {my_uniq([1,2,3])}.to_not raise_error 
         end
@@ -15,4 +15,26 @@ require 'rspec'
             expect(my_uniq([1,2,1,2,3])).to eq([1,2,3])
         end 
     end
+
+describe Array do
+    describe "#two_sum" do
+        
+        let(:pair_arr) {[-1, 0, 2, -2, 1]}
+        let(:pairless) {[5, 4, 3, 2, 1]}
+        let(:two_zero) {[-1, 5, 0, 3, 0, -3]}
+
+        context "when array is empty or has no pairs that don't sum to zero" do 
+            it "should return an empty array" do 
+                expect([].two_sum).to eq([])
+                expect(pairless.two_sum).to eq([])
+            end
+        end
+
+        it "should return a nested array that contains all pairs that sum to zero" do
+            expect(pair_arr.two_sum).to eq([[0,4], [2,3]])
+            expect(two_zero.two_sum).to eq([[2,4], [3,5]])
+        end
+
+    end
+end
 
